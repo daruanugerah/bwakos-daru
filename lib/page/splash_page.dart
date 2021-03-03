ec7a4536b693f72e8703c4dd3842f9bd97b5e576
daruanugerah <anugerahdaru@gmail.com>
2021-03-03 17:53:58 +0700

    add splash page

---
+++ lib/page/splash_page.dart
@@ -0,0 +1,70 @@
+import 'package:bwakos/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false, //note: this parameter for ios which bottom line have white space
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset('assets/splash_image.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 50,
+                left: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/logo.png'))),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'Find Cozy House\nto Stay and Happy',
+                    style: blackTextStyle.copyWith(fontSize: 24),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Stop membuang banyak waktu\npada tempat yang tidak habitable',
+                    style: greyTextStyle.copyWith(fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 210,
+                    child: RaisedButton(
+                      color: purpleColor,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(17)),
+                      child: Text(
+                        'Explore Now',
+                        style: whiteTextStyle.copyWith(fontSize: 18),
+                      ),
+                      onPressed: () {},
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

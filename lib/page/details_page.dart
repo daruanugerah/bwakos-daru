fc02e577029655762fc0fe13d53de6a6492c4f38
daruanugerah <anugerahdaru@gmail.com>
2021-03-11 22:33:19 +0700

    add detail page

---
+++ lib/page/details_page.dart
@@ -0,0 +1,303 @@
+import 'package:bwakos/theme.dart';
+import 'package:bwakos/widget/facility_item.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class DetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    launchUrl(String url) async {
+      if (await canLaunch(url)) {
+        launch(url);
+      } else {
+        throw (url);
+      }
+    }
+
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/thumbnail.png',
+              width: MediaQuery.of(context).size.width,
+              height: 350,
+              fit: BoxFit.cover,
+            ),
+            ListView(
+              children: [
+                SizedBox(
+                  height: 328,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(20),
+                      ),
+                      color: Colors.white),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      //note: TITLE
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 24),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Kurotakesso Hott',
+                                  style: blackTextStyle.copyWith(fontSize: 22),
+                                ),
+                                SizedBox(
+                                  height: 2,
+                                ),
+                                Text.rich(
+                                  TextSpan(
+                                    text: '\$52',
+                                    style:
+                                        purpleTextStyle.copyWith(fontSize: 16),
+                                    children: [
+                                      TextSpan(
+                                          text: ' / month',
+                                          style: greyTextStyle.copyWith(
+                                              fontSize: 16))
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                  color: Color(0xff989ba1),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      //note: MAIN FACILITIES
+                      Padding(
+                        padding: EdgeInsets.only(left: 24),
+                        child: Text(
+                          'Main Facilities',
+                          style: regularTextStyle.copyWith(fontSize: 16),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 24),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            FacilityItem(
+                              name: 'kitchen',
+                              imageUrl: 'assets/icon_kitchen.png',
+                              total: 2,
+                            ),
+                            FacilityItem(
+                              name: 'bedroom',
+                              imageUrl: 'assets/icon_bedroom.png',
+                              total: 3,
+                            ),
+                            FacilityItem(
+                              name: 'big lemari',
+                              imageUrl: 'assets/icon_cupboard.png',
+                              total: 3,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      //note: PHOTO
+                      Padding(
+                        padding: EdgeInsets.only(left: 24),
+                        child: Text(
+                          'Photos',
+                          style: regularTextStyle.copyWith(fontSize: 16),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Container(
+                        height: 88,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            SizedBox(
+                              width: 24,
+                            ),
+                            Image.asset(
+                              'assets/photo1.png',
+                              width: 110,
+                              height: 88,
+                              fit: BoxFit.cover,
+                            ),
+                            SizedBox(
+                              width: 18,
+                            ),
+                            Image.asset(
+                              'assets/photo2.png',
+                              width: 110,
+                              height: 88,
+                              fit: BoxFit.cover,
+                            ),
+                            SizedBox(
+                              width: 18,
+                            ),
+                            Image.asset(
+                              'assets/photo3.png',
+                              width: 110,
+                              height: 88,
+                              fit: BoxFit.cover,
+                            ),
+                            SizedBox(
+                              width: 24,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      //note: LOCATION
+                      Padding(
+                        padding: EdgeInsets.only(left: 24),
+                        child: Text(
+                          'Location',
+                          style: regularTextStyle.copyWith(fontSize: 16),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 6,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Jln. Kappan Sukses No. 20\nPalembang',
+                              style: greyTextStyle.copyWith(fontSize: 14),
+                            ),
+                            InkWell(
+                              onTap: () {
+                                launchUrl(
+                                    'https://goo.gl/maps/inXsCWyNw3FFcwHJ8');
+                              },
+                              child: Image.asset(
+                                'assets/btn_map.png',
+                                width: 40,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      //note: BUTTON
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 24,
+                        ),
+                        height: 50,
+                        width: MediaQuery.of(context).size.width - (2 * 24),
+                        child: RaisedButton(
+                          color: purpleColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(17),
+                          ),
+                          child: Text(
+                            'Book Now',
+                            style: whiteTextStyle.copyWith(fontSize: 18),
+                          ),
+                          onPressed: () {
+                            launchUrl('tel:085782756006'); //must real devaice
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 30,
+                horizontal: 24,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Image.asset(
+                      'assets/btn_back.png',
+                      width: 40,
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/btn_wishlist.png',
+                    width: 40,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
